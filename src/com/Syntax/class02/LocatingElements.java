6794e9a40c317881b534880cc43761f6639979ca
ademtural86 <ademtural86@gmail.com>
2022-10-26 14:37:32 +0200

    26/10/2022

--- src/com/Syntax/class02/LocatingElements.java
+++ src/com/Syntax/class02/LocatingElements.java
@@ -13,7 +13,9 @@ public class LocatingElements {
 //        set the path to the driver to link it with our class    on mac u dont need .exe on windows u need .exe
         System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
 // create a WebDriver instance
+
         WebDriver driver= new ChromeDriver();
+
 //        goto fb.com
         driver.get("https://www.facebook.com/");
 //        maximize the window
