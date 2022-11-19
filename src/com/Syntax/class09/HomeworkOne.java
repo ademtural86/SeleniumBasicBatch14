064ec4204419b7c8307c1f3f4a2e049feb13eb88
ademtural86 <ademtural86@gmail.com>
2022-11-19 16:49:45 +0100

    19/11/2022-2

--- src/com/Syntax/class09/HomeworkOne.java
+++ src/com/Syntax/class09/HomeworkOne.java
@@ -1,9 +1,12 @@
 package com.Syntax.class09;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class HomeworkOne {
@@ -29,6 +32,35 @@ public class HomeworkOne {
 //        maximize
         driver.manage().window().maximize();
         //  go to the website
-        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
+
+        WebElement username = driver.findElement(By.xpath("//input[@id='txtUsername']"));
+        username.sendKeys("admin");
+
+        WebElement password = driver.findElement(By.xpath("//input[@id='txtPassword']"));
+        password.sendKeys("Hum@nhrm123");
+
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@id='btnLogin']"));
+        loginBtn.click();
+
+        WebElement pimBtn = driver.findElement(By.xpath("//a[@id='menu_pim_viewPimModule']"));
+        pimBtn.click();
+
+        WebElement employeeListBtn = driver.findElement(By.xpath("//a[@id='menu_pim_viewEmployeeList']"));
+        employeeListBtn.click();
+
+        List<WebElement> secondColumn = driver.findElements(By.xpath("//table[@id='resultTable']/tbody/tr/td[2]"));
+
+        for (int i=0; i<secondColumn.size(); i++){
+            String textID=secondColumn.get(i).getText();
+
+            if (textID.equalsIgnoreCase("44869A")){
+                int rowNbr=i+1;
+                WebElement check = driver.findElement(By.xpath("//table[@id='resultTable']/tbody/tr[" + rowNbr + "]/td[1]"));
+                check.click();
+            }
+        }
+        driver.quit();
+
     }
 }
